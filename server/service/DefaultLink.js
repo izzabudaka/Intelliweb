58df758cf7f17aa2c5cd02c380ccc103c8bcdcfc
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-18 06:10:16 -0800

    default

--- server/service/DefaultLink.js
+++ server/service/DefaultLink.js
@@ -1,15 +1,7 @@
 var Card = require('../model/Card').Card;
-var getPage = require('summarizer').getPage;
-
-var uri = 'http://nodejs.org/api/documentation.html';
  
-
-
 this.get_default = function(link, callback){
-	getPage(link).then(function (data) {
-	  var card = Card()
-	  card.addTitle(data.title)
-	  card.addSubtitle(data.text)
-	  return card
-	}, console.error);
+  var card = Card()
+  card.addTitle(link)
+  return card
 }
