bc16723f0c41d52fa4ab21dc063bbb8de71a1af2
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-17 22:07:29 -0800

    Returning json

--- server/server.js
+++ server/server.js
@@ -25,7 +25,9 @@ app.get('/analyse_links', (req, res) => {
 app.post('/analyse_table', function(req, res) {
 	console.log(req.body.data);
 	grapher.get_plot(req.body.data, function(parsed){
-		res.send(parsed)
+		res.json({
+			card: parsed
+		})
 	})
 });
 
@@ -34,7 +36,9 @@ app.post('/analyse_txt', function(req, res) {
 	entity_recognizer.get_entities(req.body.text, function(entities){
 		wikipedia_service.get_entity_cards(entities, function(entity_cards){
 			console.log("MADE");
-			res.send(entity_cards)
+			res.json({
+				card: entity_cards
+			})
 		})
 	})
 });
