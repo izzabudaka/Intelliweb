f3c55438a788b74be937a4bc5e4b9ad877108794
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-18 05:25:20 -0800

    Graph

--- server/utility/Grapher.js
+++ server/utility/Grapher.js
@@ -32,3 +32,71 @@ this.get_plot = function(html, callback) {
   card.addGraph(result)
   callback(card)
 }
+
+this.plot_radar2 = function(html, callback){
+  var result = { datasets: [], labels: [] }
+  html = html.replace("<tr>","").replace("<table>", "").replace("/<table>", "")
+    .split("</tr>")
+  for(var i = 0; i < html.length; i++){
+    var current = html[i].replace("<td>", "").split("</td>")
+    if(i == 0){
+      current.slice(1,current.length).forEach(function(val){
+        result["datasets"].push({label: val.replace("<td>", ""), data: [] })
+      })
+    } else{
+      current[0] = current[0].replace("<tr>", "").trim()
+      result.labels.push(current[0])
+      for(var j = 1; j < current.length; j++){
+        result["datasets"][j-1]["data"].push(current[j].replace("<td>", "").trim())
+      }
+    }
+  }
+  console.log(JSON.stringify(result))
+}
+
+this.plot_radar = function(html, callback){
+  var $ = cheerio.load(html);
+  result = {}
+  points = {}
+  result["labels"] = []
+
+  $('tr').each(function() {
+    console.log($(this)[0].children.data)
+    var title = $(this)[0].next.data
+    console.log(title)
+    if(title != ' '){
+      console.log(data)
+      result["labels"].push(title)
+      points[title] = []
+      for(var i = 1; i < 4; i++){
+        var data = $(this)[0].children[i]
+        for(var j = 0; j < i; j++)
+          data = data.next
+        point.push(data.children[0].data)
+      }
+    }
+    else{
+      result["datasets"] = []
+      for(var i = 1; i < 4; i++){
+        var data = $(this)[0].children[i]
+        for(var j = 0; j < i; j++)
+          data = data.next
+        console.log(data.next.children[0].data)
+        result["datasets"].push({ label: data.next.children[0].data })
+      }
+    }
+  })
+  Object.keys(points).forEach(function(key){
+    for(var i = 0; i < points[key].length; i++){
+      if("data" in data["datasets"][i])
+        data["datasets"][i]["data"].push(points[key][i])
+      else{
+        data["datasets"][i]["data"] = [points[key][i]]
+      }
+    }
+  })
+
+  var card = Card()
+  card.addGraph(data)
+  callback(card)
+}
