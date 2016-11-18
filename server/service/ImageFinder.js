b5fc856200143f90b5d77895b35a9db6d4ac0219
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-17 23:10:03 -0800

    dummy images for now

--- server/service/ImageFinder.js
+++ server/service/ImageFinder.js
@@ -3,9 +3,12 @@ const ImagesClient = require('google-images');
 var client = new ImagesClient('013050441357216548743:vc_qq83pkea', 'AIzaSyCdXimkwNFZgVICv2awOhK8Tvk5e7BqNiM');
 
 this.get_image = function(title, callback){
-	client.search(title)
+	callback("https://pbs.twimg.com/media/CuQJh6tWgAEexHR.jpg")
+    /*client.search(title)
     .then(function (images) {
     	callback(images[0].url)
+        });
+        */
         /*
         [{
             "url": "http://steveangello.com/boss.jpg",
@@ -20,5 +23,4 @@ this.get_image = function(title, callback){
             }
         }]
          */
-    });
 }
