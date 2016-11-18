e4b4d3752efb26eee42ed9221b1d2f6951b12cdb
Thomas Owers <thomas@hirespace.com>
2016-11-17 18:04:12 -0800

    Added the Card model

---
+++ server/model/Card.js
@@ -0,0 +1,52 @@
+exports.Card = () => {
+    var elements = [];
+
+    var toJson = () => {
+        return JSON.stringify(elements);
+    };
+
+    var addTitle = (title) => {
+        elements.push({
+            type: 0,
+            payload: {
+                title: title
+            }
+        })
+    };
+
+    var addSubtitle = (subtitle) => {
+        elements.push({
+            type: 1,
+            payload: {
+                subtitle: subtitle
+            }
+        })
+    };
+
+    var addImage= (url) => {
+        elements.push({
+            type: 2,
+            payload: {
+                url: url
+            }
+        })
+    };
+
+    var addButton = (url, name) => {
+        elements.push({
+            type: 3,
+            payload: {
+                url: url,
+                name: name
+            }
+        })
+    };
+
+    return {
+        toJson: toJson,
+        addTitle: addTitle,
+        addSubtitle: addSubtitle,
+        addImage: addImage,
+        addButton: addButton
+    };
+};
