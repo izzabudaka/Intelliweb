7e6c2427bce40e9789352bd7d3d0d101033e7113
DIANA\Diana Ghitun <dianaa_ghm@yahoo.com>
2016-11-18 08:03:36 +0000

    get + save cards

--- client/ui.js
+++ client/ui.js
@@ -46,7 +46,7 @@
 
 	'use strict';
 
-	var _Card = __webpack_require__(1);
+	var _Card = __webpack_require__(2);
 
 	var _Card2 = _interopRequireDefault(_Card);
 
@@ -54,7 +54,7 @@
 
 	var _Navigator2 = _interopRequireDefault(_Navigator);
 
-	var _CardBar = __webpack_require__(2);
+	var _CardBar = __webpack_require__(1);
 
 	var _CardBar2 = _interopRequireDefault(_CardBar);
 
@@ -62,6 +62,87 @@
 
 /***/ },
 /* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var CardBar = function (_React$Component) {
+	  _inherits(CardBar, _React$Component);
+
+	  function CardBar(props) {
+	    _classCallCheck(this, CardBar);
+
+	    return _possibleConstructorReturn(this, (CardBar.__proto__ || Object.getPrototypeOf(CardBar)).call(this, props));
+	  }
+
+	  _createClass(CardBar, [{
+	    key: "render",
+	    value: function render() {
+	      var _this2 = this;
+
+	      var rootStyle = {
+	        height: this.props.height + "px"
+	      };
+	      console.log(this.props.height);
+	      var heights = [];
+	      for (var i = 0; i < this.props.height / 250; i++) {
+	        heights.push(React.createElement(
+	          "div",
+	          { style: { width: "100%", overflow: "scroll" } },
+	          React.createElement(
+	            "div",
+	            { style: { height: "250px", width: "750px" } },
+	            React.createElement(window.Card, { onURLClicked: function onURLClicked(url) {
+	                return _this2.props.onURLClicked(url);
+	              } }),
+	            React.createElement(window.Card, { onURLClicked: function onURLClicked(url) {
+	                return _this2.props.onURLClicked(url);
+	              } }),
+	            React.createElement(window.Card, { onURLClicked: function onURLClicked(url) {
+	                return _this2.props.onURLClicked(url);
+	              } })
+	          )
+	        ));
+	      }
+	      var remainder = this.props.height % 250;
+	      if (remainder > 0) heights.push(React.createElement("div", { style: { height: remainder + "px" } }));
+	      this.props.loaded(this);
+	      return React.createElement(
+	        "div",
+	        { style: { overflowY: "scroll" } },
+	        React.createElement(
+	          "div",
+	          { style: rootStyle },
+	          heights
+	        )
+	      );
+	    }
+	  }, {
+	    key: "updateScrollPosition",
+	    value: function updateScrollPosition(value) {
+	      if (ReactDOM.findDOMNode(this) != undefined) {
+	        console.log(ReactDOM.findDOMNode(this).scrollTop);
+	        console.log(value);
+	        ReactDOM.findDOMNode(this).scrollTop = value + "px";
+	      }
+	    }
+	  }]);
+
+	  return CardBar;
+	}(React.Component);
+
+	window.CardBar = CardBar;
+
+/***/ },
+/* 2 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -214,87 +295,6 @@
 	}(React.Component);
 
 	window.Card = Card;
-
-/***/ },
-/* 2 */
-/***/ function(module, exports) {
-
-	"use strict";
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var CardBar = function (_React$Component) {
-	  _inherits(CardBar, _React$Component);
-
-	  function CardBar(props) {
-	    _classCallCheck(this, CardBar);
-
-	    return _possibleConstructorReturn(this, (CardBar.__proto__ || Object.getPrototypeOf(CardBar)).call(this, props));
-	  }
-
-	  _createClass(CardBar, [{
-	    key: "render",
-	    value: function render() {
-	      var _this2 = this;
-
-	      var rootStyle = {
-	        height: this.props.height + "px"
-	      };
-	      console.log(this.props.height);
-	      var heights = [];
-	      for (var i = 0; i < this.props.height / 250; i++) {
-	        heights.push(React.createElement(
-	          "div",
-	          { style: { width: "100%", overflow: "scroll" } },
-	          React.createElement(
-	            "div",
-	            { style: { height: "250px", width: "750px" } },
-	            React.createElement(window.Card, { onURLClicked: function onURLClicked(url) {
-	                return _this2.props.onURLClicked(url);
-	              } }),
-	            React.createElement(window.Card, { onURLClicked: function onURLClicked(url) {
-	                return _this2.props.onURLClicked(url);
-	              } }),
-	            React.createElement(window.Card, { onURLClicked: function onURLClicked(url) {
-	                return _this2.props.onURLClicked(url);
-	              } })
-	          )
-	        ));
-	      }
-	      var remainder = this.props.height % 250;
-	      if (remainder > 0) heights.push(React.createElement("div", { style: { height: remainder + "px" } }));
-	      this.props.loaded(this);
-	      return React.createElement(
-	        "div",
-	        { style: { overflowY: "scroll" } },
-	        React.createElement(
-	          "div",
-	          { style: rootStyle },
-	          heights
-	        )
-	      );
-	    }
-	  }, {
-	    key: "updateScrollPosition",
-	    value: function updateScrollPosition(value) {
-	      if (ReactDOM.findDOMNode(this) != undefined) {
-	        console.log(ReactDOM.findDOMNode(this).scrollTop);
-	        console.log(value);
-	        ReactDOM.findDOMNode(this).scrollTop = value + "px";
-	      }
-	    }
-	  }]);
-
-	  return CardBar;
-	}(React.Component);
-
-	window.CardBar = CardBar;
 
 /***/ },
 /* 3 */
