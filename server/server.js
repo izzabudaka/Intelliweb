54fcb4b6ffcca9314f202f5ec4290535a1529de4
Izz Abudaka <izz.abudaka@gmail.com>
2016-11-17 17:03:01 -0800

    analyse text workflow

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    fibrous = require('fibrous');
+    fibrous = require('fibrous'),
+    entity_recognizer = require('./utility/EntityRecognizer'),
+    wikipedia_service = require('./service/Wikipedia');
 
 var port = process.env.PORT || 3000;
 
@@ -15,6 +17,17 @@ app.use(fibrous.middleware);
 app.post('/api', function(req, res) {
     console.log(req.body);
 });
+
+app.post('/analyse_txt', function(req, res) {
+	console.log(req.body.text);
+	entity_recognizer.get_entities(req.body.text, function(entities){
+		wikipedia_service.get_entity_cards(entities, function(entity_cards){
+			console.log("MADE")
+			res.send(entity_cards)
+		})
+	})
+});
+
 
 app.listen(port, function () {
     console.log('Server listening on port ' + port + '!');
---
+++ server/service/Wikipedia.js
@@ -0,0 +1,84 @@
+var wikipedia = require("wikipedia-js");
+
+var async = require('async');
+
+var get_person_card = function(title, callback){
+	var options = {query: title, format: "html", summaryOnly: true};
+	wikipedia.searchArticle(options, function(err, wiki_text){
+	    if(err){
+	      console.log("An error occurred[query=%s, error=%s]", query, err);
+	      return;
+	    }
+    	callback(wiki_text)
+    })
+}
+
+var get_location_card = function(title, callback){
+	var options = {query: title, format: "html", summaryOnly: true};
+	wikipedia.searchArticle(options, function(err, wiki_text){
+	    if(err){
+	      console.log("An error occurred[query=%s, error=%s]", query, err);
+	      return;
+	    }
+    	callback(wiki_text)
+    })
+}
+
+var get_organization_card = function(title, callback){
+	var options = {query: title, format: "html", summaryOnly: true};
+	console.log(options)
+	wikipedia.searchArticle(options, function(err, wiki_text){
+	    if(err){
+	      console.log("An error occurred[query=%s, error=%s]", query, err);
+	      return;
+	    }
+    	callback(wiki_text)
+    })
+}
+
+this.get_entity_cards = function(entities, callback){
+	result = []
+	var processed = 0
+	var last_processed = processed
+	var size = 0
+	var keys = Object.keys(entities)
+	async.forEach(keys, function(key){
+		size += entities[key].length
+	})
+
+	var addToResult = function(card){
+		result.push(card.replace(/<(?:.|\n)*?>/gm, ''))
+		last_processed = processed
+		processed++
+		if(processed == size)
+			callback(result)
+	}
+	
+	if(entities["ORGANIZATION"] != undefined){
+		entities["ORGANIZATION"] = new Set(entities["ORGANIZATION"])
+		async.forEach(entities["ORGANIZATION"], function(val){
+			console.log(val)
+			get_organization_card(val, addToResult)			
+		})
+	}
+
+	if(entities["PERSON"] != undefined){
+		entities["PERSON"] = new Set(entities["PERSON"])
+		async.forEach(entities["PERSON"], function(val){
+			console.log(val)
+			get_person_card(val, addToResult)			
+		})
+	}
+
+	if(entities["LOCATION"] != undefined){
+		entities["LOCATION"] = new Set(entities["LOCATION"])
+		async.forEach(entities["LOCATION"], function(val){
+			console.log(val)
+			get_location_card(val, addToResult)			
+		})
+	}
+
+	setTimeout(function(){
+		callback(result)
+	}, 10*1000);
+}
