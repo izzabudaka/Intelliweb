daf022c1fdea8e68857be33be0e5e8b5c168c41b
DIANA\Diana Ghitun <dianaa_ghm@yahoo.com>
2016-11-18 00:22:32 +0000

    return object with link & position

--- client/bridge.js
+++ client/bridge.js
@@ -2,3 +2,16 @@ const {ipcRenderer} = require('electron');
 window.onscroll = function() {
       ipcRenderer.sendToHost('scrolling',window.scrollY);
 };
+
+var list = Array.prototype.slice.call(document.getElementsByTagName('a'),0); 
+
+var obj = list.filter(function(reference){ return reference.href.length!=0 && (reference.getBoundingClientRect().top!=0 && reference.getBoundingClientRect().left!=0);})
+.map(function(reference){
+      
+      return {
+            link: reference.href,
+            posX: reference.getBoundingClientRect().left,
+            posY: reference.getBoundingClientRect().top
+      };
+      
+});
